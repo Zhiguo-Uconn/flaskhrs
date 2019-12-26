ada3e0e9f1a58174c8bb3174437649c42f62e99f
Zhiguo Wang <zhiguo.wang@uconn.edu>
2019-12-26 05:38:44 -0500

    User module is completed, possible new functions: update and delete user

--- flaskhrs/routes.py
+++ flaskhrs/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskhrs.forms import RegistrationForm, LoginForm
 from flaskhrs.models import User, Doctor, Patient
 from flaskhrs import app, bcrypt, db
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 clients = [
@@ -39,7 +39,8 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(role=form.role.data, email=form.email.data, password=hashed_password)
+        user = User(role=form.role.data, display_name=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}! You are now able to login.', 'success')
@@ -56,9 +57,20 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
